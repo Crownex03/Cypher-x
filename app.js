7ecb6485395d64c18c090683b1e0c276a226d555
Crownex03 <crownextelecom@gmail.com>
2026-05-01 22:08:57 +0100

    Add configuration for WhatsApp Bot

---
+++ app.js
@@ -0,0 +1,12 @@
+{
+  "name": "WhatsApp Bot",
+  "description": "Simple WhatsApp Bot",
+  "repository": "https://github.com/YOUR_USERNAME/YOUR_REPO_NAME",
+  "keywords": ["whatsapp", "bot", "nodejs"],
+  "env": {
+    "SESSION_PATH": {
+      "description": "Session storage path",
+      "value": "./session"
+    }
+  }
+}
